6d4fc758bf219f3509da796571be1543e00e324a
clicea-asti <clicea@asti.com>
2019-07-01 10:58:25 -0600

    Add project files.

---
+++ Asti.WordSpecWeb/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Asti.WordSpecWeb
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteTable.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+        }
+    }
+}
